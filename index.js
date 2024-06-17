c101358d80994213c03ab2b69523f64ebd4bd9c8
geekcoldhand <horatiousaharris@gmail.com>
2024-06-16 21:04:55 -0400

    init commit with skeletal struc

---
+++ index.js
@@ -0,0 +1,7 @@
+const letterEmoji = document.getElementById('letter').addEventListener('click', handleLetterClicked);
+const letter = "/img/letter.png";
+
+function handleLetterClicked(event) {
+    console.log(event.target);
+
+}
