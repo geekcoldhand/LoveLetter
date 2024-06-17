c010ab885de56ecda3dcae1a7635fcef5a73c4f2
geekcoldhand <horatiousaharris@gmail.com>
2024-06-16 21:21:38 -0400

    testing touch on iphone

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const letterEmoji = document.getElementById('letter').addEventListener('click', handleLetterClicked);
+const letterEmoji = document.getElementById("letter");
 const letter = "/img/letter.png";
-
+letterEmoji.addEventListener("click", handleLetterClicked());
+letterEmoji.addEventListener("touchstart", handleLetterClicked());
 function handleLetterClicked(event) {
-    console.log(event.target);
-
+  console.log(event.target);
 }
